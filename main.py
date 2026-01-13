242faf5511bb96635508fc6e817a1fb7990891b4
ycho5592 <116114988+ycho5592@users.noreply.github.com>
2026-01-13 09:53:32 -0500

    Activating close window

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
 from constants import*
+from logger import log_state
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
