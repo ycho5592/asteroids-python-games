92b138ff6049f42d2897d4db6edb2451af59bc9a
ycho5592 <116114988+ycho5592@users.noreply.github.com>
2026-01-13 12:02:46 -0500

    game over when colliding

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        sum_raidus = self.radius + other.radius
+        distance = pygame.math.Vector2.distance_to(self.position, other.position)
+        if distance <= sum_raidus:
+            return True
+        return False
