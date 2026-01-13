083712949a7969fb364688732d617f965316b611
ycho5592 <116114988+ycho5592@users.noreply.github.com>
2026-01-13 11:30:57 -0500

    grouping instead of manually calling

--- main.py
+++ main.py
@@ -8,20 +8,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+    # game loop
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
-        
+
         dt = clock.tick(60) / 1000
-        # print(dt)
         
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}!")
